baf477febd16eb9fb3955fd12367c2115e7ce35b
YOUR NAME HERE <ksj4@sfu.ca>
2025-06-19 19:01:24 -0700

    Added paths to Contact and Project pages iun order to impot them in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import About from './components/pages/about/About';
+import Contact from './components/pages/contact/Contact';
+import Project from './components/pages/project-skills-resume/Project';
 
 function App() {
     return (
         <div>
             <About />
+           
         </div>
     );
 }
