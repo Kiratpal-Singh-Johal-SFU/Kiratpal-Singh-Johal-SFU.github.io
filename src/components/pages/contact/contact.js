5ff855b0e36c1e9b8b9af49184bb64a22b202a39
YOUR NAME HERE <ksj4@sfu.ca>
2025-06-19 19:38:18 -0700

    Made chnages to layout and divsions to make sure it matched the About page style and colour

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -3,18 +3,22 @@ import '../../styles/Contact.css';
 
 function Contact() {
     return (
-        <div className="contact">
-            <div className="info">
-                <h2>Contact Information</h2>
-                <p>Email: <a href="mailto:jkiratpal@gmail.com">jkiratpal@gmail.com</a></p>
-                <p>City: Vancouver, Canada</p>
-                <p>School: Simon Fraser University (SFU)</p>
-                <div className="social_links">
-                    <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">LinkedIn</a>
-                    <a href="https://www.instagram.com" target="_blank" rel="noreferrer">Instagram</a>
+        <>
+            <div className="contact">
+                <div className="contact-info">
+                    <h2>Contact Information</h2>
+                    <p>Email: <a href="mailto:jkiratpal@gmail.com">jkiratpal@gmail.com</a></p>
+                    <p>City: Vancouver, Canada</p>
+                    <p>School: Simon Fraser University (SFU)</p>
                 </div>
             </div>
-        </div>
+
+            <div className="social_icons">
+                <a href="https://github.com/Kiratpal-Singh-Johal-SFU" target="_blank" rel="noreferrer">GitHub</a>
+                <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href="YOUR_RESUME_LINK" target="_blank" rel="noreferrer">Resume</a>
+            </div>
+        </>
     );
 }
 
