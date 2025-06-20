215c40104acd63eb1c90963fa4f1e6725e31c3f3
YOUR NAME HERE <ksj4@sfu.ca>
2025-06-19 21:16:35 -0700

    Updated App.js to import  react router and set About as first page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/pages/about/About';
 import Contact from './components/pages/contact/Contact';
 import Project from './components/pages/project-skills-resume/Project';
 
 function App() {
     return (
-        <div>
-            <About />
-            <Project />
-            <Contact />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<About />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Project />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
     );
 }
 
