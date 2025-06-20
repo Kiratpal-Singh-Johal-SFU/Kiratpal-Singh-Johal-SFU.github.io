a8947d7ef08f32d76d63bf0cd666d136adc57772
YOUR NAME HERE <ksj4@sfu.ca>
2025-06-19 17:23:11 -0700

    Added image to About page

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './About.css';
-
+import profilePhoto from '../../assets/headshot.jpg';
 function About() {
     return (
         <>
@@ -8,6 +8,9 @@ function About() {
                 <div className="bio">
                     <p>I'm a Computer Science student at SFU from India.<br/>
                     I love snowboarding, tennis, and learning new sports!</p>
+                </div>
+                <div className="image">
+                    <img src={profilePhoto} alt="Profile" />
                 </div>
                 <div className="name">
                     <h1>Kiratpal<br/>Singh <br/> Johal</h1>
