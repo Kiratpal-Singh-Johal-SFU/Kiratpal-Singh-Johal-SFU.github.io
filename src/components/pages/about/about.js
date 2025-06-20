f3c62a72e10a5799a8ffbe7671f56c7873e3abe5
YOUR NAME HERE <ksj4@sfu.ca>
2025-06-19 17:18:28 -0700

    Added Bio to about page and name

---
+++ src/components/pages/about/about.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <>
+            <div className="about">
+                <div className="bio">
+                    <p>I'm a Computer Science student at SFU from India.<br/>
+                    I love snowboarding, tennis, and learning new sports!</p>
+                </div>
+                <div className="name">
+                    <h1>Kiratpal<br/>Singh <br/> Johal</h1>
+                </div>
+            </div>
+                <div className="empty">
+                </div>
+        </>
+    );
+}
+
+export default About;
